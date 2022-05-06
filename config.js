29f51f0cc99c8b4109646318d3729bd496beef8d
Phạm Trường <59144820+pamtrg@users.noreply.github.com>
2022-05-06 22:58:54 +0700

    Add files via upload
    Create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Nguỵt';
+const giftUrl = 'https://www.facebook.com/pamtrg/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng đằng ấy';
+const contentCard = 'Có thể sinh nhật của bạn sẽ tuyệt vời như vai trò của bạn trong cuộc sống này vậy <3.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
